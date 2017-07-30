3ffd0edb3b293cdf915c9bb1c72fb01e68606351
Rasakhatskiy <saharok.maks@gmail.com>
2017-07-30 21:00:11 +0300

    new project

---
+++ globals.h
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////
+//ONLY "EXTERN" "CONST" & PROTOTYPES
+/////////////////////////////////////////////
+#pragma once
+#include <SFML\Graphics.hpp>
+using namespace sf;
+
+const sf::VideoMode _Mode1600x900_(1600, 900, 32);
+
+
+void menu();
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <SFML\Graphics.hpp>
+#include <iostream>
+#include "globals.h"
+
+using namespace sf;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+//todo
+void menu() {
+
+}
+int main() {
+	RenderWindow window(_Mode1600x900_, "Defenders");
+}
